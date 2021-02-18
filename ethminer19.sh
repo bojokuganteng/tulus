dddbe64c9c0a3abc8106fb7f9a3a9d2dc67e8c11
Maychan212 <73437897+Maychan212@users.noreply.github.com>
2021-02-18 23:21:16 +0700

    Update ethminer19.sh

--- ethminer19.sh
+++ ethminer19.sh
@@ -3,4 +3,4 @@
 #################################
 ## Begin of user-editable part ##
 ############################
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x4459d89f3017298cf70dbac0482bb06cb98021b5.Widiantoadithya@eu1.ethermine.org:5555
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x4459d89f3017298cf70dbac0482bb06cb98021b5.Widiantoadithya@eu1.ethermine.org:4444
